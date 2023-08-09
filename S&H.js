0eba1299f2322c1208755114a97c17379b8b69cf
Aman Chaurasia <141608610+Aman-Chaurasia-official@users.noreply.github.com>
2023-08-09 06:21:59 +0000

    Stack and Heap Memory

---
+++ S&H.js
@@ -0,0 +1,16 @@
+let a = "Aman Chaurasia"
+let b = a
+b = "Atul Chaurasia"
+console.log(a)
+console.log(b)
+console.log("..........")
+let User1 = {
+    Name : "Aman"
+}
+
+let User2 = User1
+
+User2.Name = "Atul"
+
+console.log(User1)
+console.log(User2)
