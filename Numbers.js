b5a6b17427485ae54e0fb3b4aac090ef2148a61e
Aman Chaurasia <141608610+Aman-Chaurasia-official@users.noreply.github.com>
2023-08-09 12:16:45 +0000

    Number and Maths Done

---
+++ Numbers.js
@@ -0,0 +1,26 @@
+// const a = 300
+// console.log(a);
+// const aa = new Number(423.34543)
+// console.log(aa);
+
+// console.log(aa.toFixed(3
+//     ))
+// console.log(aa.toPrecision(2));
+// console.log(aa.toString())
+
+const Hundred = 1000000;
+// console.log(Hundred.toLocaleString('en-IN'))
+
+//=========MATHS==============\\
+// console.log(Math.ceil(4.7))
+// console.log(Math.round(4.5))
+// console.log(Math.abs(-4));
+// console.log(Math.floor(4.45));
+
+// console.log(Math.random());
+// console.log((Math.random() * 10)+ 1);
+console.log(Math.floor(Math.random() * 10)+ 1);
+
+const Min = 5
+const Max = 20
+console.log(Math.floor(Math.random() * (Max- Min))+ Min);
