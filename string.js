606d50000ccb07948d1fcd3b47276aec6b9fe47c
Aman Chaurasia <141608610+Aman-Chaurasia-official@users.noreply.github.com>
2023-08-09 09:15:25 +0000

    String

---
+++ string.js
@@ -0,0 +1,18 @@
+const a = new String ("Aman  ")
+// console.log(a.replace("A","b"))
+// console.log(a.toLowerCase)
+// console.log(a.toUpperCase)
+// console.log(a.indexOf("n"));
+// console.log(a.charAt(2))
+// console.log(a.slice('1','4'))
+console.log(a.split('a'));
+console.log(a.toWellFormed());
+console.log(a.trimEnd());
+// let url = "https://Amanchaurasia%20sss.com"
+// console.log(url.includes('Aman'))
+
+// const worldString = "Hello, world";
+// console.log(worldString.at(0));
+// console.log(worldString.bold());
+// console.log(worldString.blink());
+// console.log(worldString.big());
