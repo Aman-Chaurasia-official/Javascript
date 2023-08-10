0916e5df51f5086a4ca09e770f476c06f8dadcd2
Aman Chaurasia <141608610+Aman-Chaurasia-official@users.noreply.github.com>
2023-08-10 18:50:51 +0000

    Array and Objects Done

--- Objects.js
+++ Objects.js
@@ -17,8 +17,32 @@ const user = {
 // Object.freeze(user)
 // console.log(user);
 
-user.greeting = function(){
-    console.log(`Hello ${this.name}`);
-}
-console.log(user.greeting);
+// user.greeting = function(){
+//     console.log(`Hello ${this.name}`);
+// }
+// console.log(user.greeting);
 // console.log(user);
+
+
+const Data = {
+    1 : "a",
+    Full : {
+        a : "2",
+        Name : {
+            3: "c",
+            d: "4"
+        }
+    }
+}
+
+// console.log(Data.Full.Name);
+
+const obj1 = {a : "123",b : "456"}
+const obj2 = {c : "789",d : "101"}
+// const obj3 = Object.assign({},obj1,obj2)
+const obj3 = {...obj1,...obj2}
+console.log(obj3);
+console.log(Object.keys(obj1));
+console.log(Object.values(obj1));
+console.log(Object.entries(obj1));
+console.log(obj1.hasOwnProperty("a"));
