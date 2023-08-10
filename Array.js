5db814df7f4acb631c6535ecb2aec8159c75ab22
Aman Chaurasia <141608610+Aman-Chaurasia-official@users.noreply.github.com>
2023-08-10 07:13:33 +0000

    Arrays

---
+++ Array.js
@@ -0,0 +1,18 @@
+const myArr = [1,2,3,4,5,6]
+console.log(myArr);
+// console.log(myArr.push(12));
+// console.log(myArr)
+// console.log(myArr.pop(3))
+// console.log(myArr)
+// myArr.unshift(0)
+// myArr.shift()
+// console.log(myArr);
+
+// console.log(myArr.slice(1,3));
+// console.log(myArr)
+
+// console.log(myArr.splice(1,3));
+// console.log(myArr)
+console.log(myArr.includes(4));
+console.log(myArr.indexOf(3));
+console.log(myArr[2]);
---
+++ Array_02.js
@@ -0,0 +1,18 @@
+const Name1 = ["Aman","Atul","Suraj"]
+const Name2 = ["Neha","Archana","Ankit"] 
+
+// Name1.push(Name2)
+// console.log(Name1);
+// const All_Name = Name1.concat(Name2)
+// console.log(All_Name);
+
+// const Num_Arr = [1,2,4,[3,4,9],4,5,[44,33,[21,33,32],34],5]
+// console.log(Num_Arr.flat(Infinity));
+
+console.log(Array.isArray(Name1));
+console.log(Array.from("Aman"))
+
+const a1 = 223;
+const a2 = 22;
+const a3 =44;
+console.log(Array.of(a1,a2,a3));
