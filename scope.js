ade5ef2782a3be0665f21132b39b7da20c465693
Aman Chaurasia <141608610+Aman-Chaurasia-official@users.noreply.github.com>
2023-08-11 12:25:31 +0000

    scope

--- scope.js
+++ scope.js
@@ -1,18 +1,51 @@
-let a = 10;
-var b = 420;
-const c = 30;
+// let a = 10;
+// var b = 420;
+// const c = 30;
 
-if (true) {
-  let a = 10;
-  var b = 20;
-  const c = 30;
-  console.log("BLOCK SCOPE : ",a);
-console.log("BLOCK SCOPE : ",b);
-console.log("BLOCK SCOPE : ",c);
+// if (true) {
+//   let a = 10;
+//   var b = 20;
+//   const c = 30;
+//   console.log("BLOCK SCOPE : ",a);
+// console.log("BLOCK SCOPE : ",b);
+// console.log("BLOCK SCOPE : ",c);
 
 
+// }
+
+// console.log("GLOBAL SCOPE : ",a);
+// console.log("GLOBAL SCOPE : ",b);
+// console.log("GLOBAL SCOPE : ",c);
+
+// function one() {
+//   const username = "Aman"
+//   function two(){
+//     const ID = 1234
+//       console.log(ID);
+//     console.log(username);
+//   }
+//   two()
+// }
+// one()
+
+// if(true){
+//   const username = "Aman"
+//   if(username === "Aman"){
+//     const website = 12345
+//     console.log(username)
+//   }
+//   // console.log(website);
+// }
+
+console.log(addOne(5));
+function addOne(num){
+  return num + 1
 }
 
-console.log("GLOBAL SCOPE : ",a);
-console.log("GLOBAL SCOPE : ",b);
-console.log("GLOBAL SCOPE : ",c);
+
+
+const addTwo = function(num){
+  return num + 2
+}
+
+console.log(addTwo(5));
