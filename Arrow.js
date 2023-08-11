31885e69029d46944dde47346fedf3a7219eafab
Aman Chaurasia <141608610+Aman-Chaurasia-official@users.noreply.github.com>
2023-08-11 15:21:03 +0000

    Arrow

---
+++ Arrow.js
@@ -0,0 +1,46 @@
+const user = {
+    username : "Aman",
+    price : "345",
+
+    welcomeMessage: function() {
+        console.log(`${this.username}, welcome to this site`);
+        console.log(this);
+    }
+}
+
+// user.welcomeMessage()
+
+user.username = "Atul"
+// user.welcomeMessage()
+
+function one(){
+    name : "Aman", 
+    ss = 3
+    console.log(this.username)
+}
+// one()
+
+ const two = () =>{
+    name : "Aman", 
+    ss = 3
+    console.log(this)
+}
+
+// two()
+
+// const addTwoNum = (num1,num2) => {
+//     return num1 + num2
+// }
+
+// const addTwoNum = (num1,num2) => (num1 + num2)
+
+// const addTwoNum = (num1,num2) => (username = "Aman")
+
+
+// console.log(addTwoNum(2,5))
+
+(function chaia(user){
+    console.log(`DB CONNECTED TO SERVER ${user}`);
+})('Aman');
+
+( (user) => console.log(`DB SERVER CONNECTED TO ${user}`) )('Aman Server');
